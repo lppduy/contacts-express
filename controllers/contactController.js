e5ca5898c0235c5750ebd92ec584b4c8e7ec3bf1
lppduy <lephamphuongduy@gmail.com>
2024-03-24 20:06:40 +0700

    feat: add getContacts

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,11 +1,13 @@
 const asyncHandler = require('express-async-handler');
-
+const Contact = require('../models/contactModel');
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access Public
 exports.getContacts = asyncHandler(
   async (req, res, next) => {
-    res.status(200).json({ message: 'Get all contacts' });
+
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
   });
 
 //@desc Create a contact
