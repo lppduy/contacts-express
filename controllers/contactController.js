57f7dae6669526f48935d02d6108eea9122e9670
lppduy <lephamphuongduy@gmail.com>
2024-03-24 20:52:19 +0700

    feat: finish contact endpoint and remove asyncHandler

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,71 +1,120 @@
-const asyncHandler = require('express-async-handler');
 const Contact = require('../models/contactModel');
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access Public
-exports.getContacts = asyncHandler(
-  async (req, res, next) => {
-
-    const contacts = await Contact.find();
-    res.status(200).json(contacts);
-  });
+exports.getContacts = (req, res, next) => {
+  Contact.find()
+    .then(contacts => {
+      res.status(200).json(contacts);
+    })
+    .catch(err => {
+      res.status(400);
+      res.json({
+        title: "Something went wrong",
+        message: err.message,
+        stackTrace: err.stack
+      }) // error: email existed or something else -> handle later
+    })
+}
 
 //@desc Create a contact
 //@route POST /api/contacts
 //@access Public
-exports.createContact = asyncHandler(
-  async (req, res, next) => {
+exports.createContact = (req, res, next) => {
 
-    const { name, email, phone } = req.body;
+  const { name, email, phone } = req.body;
 
-    if (!name || !email || !phone) {
-      res.status(400);
-      throw new Error('Please fill in all fields');
-    }
-    const contact = await Contact.create({
-      name,
-      email,
-      phone
-    });
-    res.status(201).json(contact);
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error('Please fill in all fields');
   }
-);
+  const newContact = new Contact({
+    name,
+    email,
+    phone
+  });
+
+  newContact.save().then(contact => {
+    res.status(201).json(contact);
+  }).catch(err => {
+    res.status(400);
+    res.json({
+      title: "Something went wrong",
+      message: err.message,
+      stackTrace: err.stack
+    })
+  })
+}
+
 
 //@desc Update a contact
 //@route PUT /api/contacts/:id
 //@access Public
-exports.updateContact = asyncHandler(
-  async (req, res, next) => {
-    res.status(200).json({ message: `Update contact with id of ${req.params.id}` });
-  }
-);
+exports.updateContact = (req, res, next) => {
+  Contact.findById(req.params.id)
+    .then(contact => {
+      if (contact) {
+        contact.name = req.body.name || contact.name;
+        contact.email = req.body.email || contact.email;
+        contact.phone = req.body.phone || contact.phone;
+
+        contact.save().then(contact => {
+          res.status(200).json(contact);
+        }).catch(err => {
+          res.status(400);
+          res.json({
+            title: "Something went wrong",
+            message: err.message,
+            stackTrace: err.stack
+          })
+        })
+      }
+    }
+    ).catch(err => {
+      res.status(404);
+      res.json({
+        title: "Contact Not Found",
+        message: err.message,
+        stackTrace: err.stack
+      });
+    })
+};
 
 //@desc Get a contact
 //@route GET /api/contacts/:id
 //@access Public
-exports.getContact = asyncHandler(
-  async (req, res, next) => {
-    Contact.findById(req.params.id)
-      .then(contact => {
-        res.status(200).json(contact);
-      })
-      .catch(err => {
-        res.status(404);
-        res.json({
-          title: "Contact Not Found",
-          message: err.message,
-          stackTrace: err.stack
-        });
+exports.getContact = (req, res, next) => {
+  Contact.findById(req.params.id)
+    .then(contact => {
+      res.status(200).json(contact);
+    })
+    .catch(err => {
+      res.status(404);
+      res.json({
+        title: "Contact Not Found",
+        message: err.message,
+        stackTrace: err.stack
       });
-  }
-);
+    });
+}
+
 
 //@desc Delete a contact
 //@route DELETE /api/contacts/:id
 //@access Public
-exports.deleteContact = asyncHandler(
-  async (req, res, next) => {
-    res.status(200).json({ message: `Delete contact with id of ${req.params.id}` });
-  }
-);
+exports.deleteContact = (req, res, next) => {
+  Contact.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(200).json({ message: 'Contact removed' });
+    })
+    .catch(err => {
+      res.status(404);
+      res.json({
+        title: "Contact Not Found",
+        message: err.message,
+        stackTrace: err.stack
+      });
+    });
+}
+
 
