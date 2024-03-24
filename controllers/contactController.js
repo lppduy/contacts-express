25b5ef7afb2154507bac88d4f1193024f122ab39
lppduy <lephamphuongduy@gmail.com>
2024-03-24 17:36:58 +0700

    feat: add express async handler

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,42 +1,54 @@
+const asyncHandler = require('express-async-handler');
+
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access Public
-exports.getContacts = (req, res, next) => {
-  res.status(200).json({ message: 'Get all contacts' });
-};
+exports.getContacts = asyncHandler(
+  async (req, res, next) => {
+    res.status(200).json({ message: 'Get all contacts' });
+  });
 
 //@desc Create a contact
 //@route POST /api/contacts
 //@access Public
-exports.createContact = (req, res, next) => {
+exports.createContact = asyncHandler(
+  async (req, res, next) => {
 
-  const { name, email, phone } = req.body;
+    const { name, email, phone } = req.body;
 
-  if (!name || !email || !phone) {
-    res.status(400);
-    throw new Error('Please fill in all fields');
+    if (!name || !email || !phone) {
+      res.status(400);
+      throw new Error('Please fill in all fields');
+    }
+    res.status(200).json({ message: 'Create a contact' });
   }
-  res.status(200).json({ message: 'Create a contact' });
-};
+);
 
 //@desc Update a contact
 //@route PUT /api/contacts/:id
 //@access Public
-exports.updateContact = (req, res, next) => {
-  res.status(200).json({ message: `Update contact with id of ${req.params.id}` });
-};
+exports.updateContact = asyncHandler(
+  async (req, res, next) => {
+    res.status(200).json({ message: `Update contact with id of ${req.params.id}` });
+  }
+);
 
 //@desc Get a contact
 //@route GET /api/contacts/:id
 //@access Public
-exports.getContact = (req, res, next) => {
-  res.status(200).json({ message: `Get contact with id of ${req.params.id}` });
-};
+exports.getContact = asyncHandler(
+  async (req, res, next) => {
+    res.status(200).json({ message: `Get contact with id of ${req.params.id}` });
+  }
+);
 
 //@desc Delete a contact
 //@route DELETE /api/contacts/:id
 //@access Public
-exports.deleteContact = (req, res, next) => {
-  res.status(200).json({ message: `Delete contact with id of ${req.params.id}` });
-};
+exports.deleteContact = asyncHandler(
+  async (req, res, next) => {
+    res.status(200).json({ message: `Delete contact with id of ${req.params.id}` });
+  }
+);
 
