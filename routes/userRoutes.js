69c572d6814a6f18a8ae5a1a6d0d4c392dcc7862
lppduy <lephamphuongduy@gmail.com>
2024-03-25 13:00:48 +0700

    add validateToken

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 const expres = require('express');
 const router = expres.Router();
 const { registerUser, loginUser, getCurrentUser } = require('../controllers/userController');
+const { validateToken } = require('../middlewares/validateTokenHandler');
 
 router.post('/register', registerUser);
 
 router.post('/login', loginUser);
 
-router.post('/current', getCurrentUser);
+router.post('/current', validateToken, getCurrentUser);
 
 module.exports = router;
